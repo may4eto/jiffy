8c2cb8c305b919162c526497dfb5c9ca9a886e22
Maya Mircheva <may4eto@hotmail.com>
2021-02-22 18:49:14 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import Gif from './Gif';
 import loader from './images/loader.svg';
+import clearButton from './images/close-icon.svg';
 
-const Header = () => (
+const randomChoice = arr => {
+  const randIndex = Math.floor(Math.random() * arr.length)
+  return arr[randIndex]
+}
+
+const Header = ({clearSearch, hasResults}) => (
   <div className="header grid">
+    {/* if we have results, show the clear button, otherwise show the title */}
+    {hasResults ? (
+      <button onClick={clearSearch}>
+        <img src={clearButton} alt="Clear Search" />
+      </button>
+    ) : (
       <h1 className="title">Jiffy</h1>
+    )}
   </div>
 );
 
@@ -22,34 +36,69 @@ class App extends Component {
       loading: false,
       searchTerm: '',
       hintText: '',
+      gifs: []
     };
   }
+
+  searchGiphy = async searchTerm => {
+    this.setState({loading: true});
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=QYsVOSHo81Of3SVD5C5BKtKHVWjhLMua&q=${searchTerm}&limit=100&offset=0&rating=g&lang=en`);
+      const {data} = await response.json();
+      if (!data.length) {
+        throw `Nothing found for ${searchTerm}`;
+      }
+      const randomGif = randomChoice(data);
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gifs: [...prevState.gifs, randomGif],
+        loading: false,
+        hintText: `Hit enter to see more ${searchTerm}`
+      }))
+    } catch (error) {
+      this.setState((prevState, props) => ({
+        ...prevState,
+        hintText: error,
+        loading: false
+      }));
+    }
+  };
 
   handleChange = event => {
     const {value} = event.target;
     this.setState((prevState, props) => ({
       ...prevState,
       searchTerm: value,
-      hintText: value.length > 2 ? `Hit enter to search ${value}` : ''
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : '',
     }));
   };
 
   handleKeyPress = event => {
     const {value} = event.target;
     if (value.length > 2 && event.key === 'Enter') {
-      
+      this.searchGiphy(value);
     }
   };
 
+  clearSearch = () => {
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: '',
+      hintText: '',
+      gifs: []
+    }))
+    this.textInput.focus();
+  };
+
   render() {
-    const {searchTerm} = this.state;
-   
+    const {searchTerm, gifs} = this.state;
+    const hasResults = gifs.length;
     return (
       <div className="page">
-        <Header />
-
+        <Header clearSearch={this.clearSearch} hasResults={hasResults}/>
         <div className="search grid">
           {/* our stack of gif images */}
+          {this.state.gifs.map(gif => (<Gif {...gif}/>))}
           <input
             className="input grid-item"
             placeholder="Type something"
